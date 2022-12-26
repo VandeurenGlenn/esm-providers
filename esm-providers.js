ce35ba62484cdf3ea549c806eef241400f2b0f44
vandeurenglenn <vandeurenglenn@gmail.com>
2022-12-26 18:38:07 +0100

    Simplify & add packageJson option

--- esm-providers.js
+++ esm-providers.js
@@ -1,6 +1,23 @@
+import getPackageJson from './lib/get-package-json.js'
 import * as providers from './lib/providers.js'
+import { join } from 'path'
 
 export const availableProviders = Object.entries(providers).map(provider => (provider[0]))
+
+const providerResult = async (name, destination, packageJson = {}) => {
+  
+  if (Object.keys(packageJson).length === 0) packageJson = await getPackageJson(join(destination, name))
+
+  const { version, exports, dependencies, devDependencies } = packageJson
+  return {
+    name,
+    version,
+    exports,
+    dependencies,
+    devDependencies,
+    path: join(destination, name)
+  }
+}
 
 /**
  * 
@@ -8,12 +25,18 @@ export const availableProviders = Object.entries(providers).map(provider => (pro
  * @param {options} options {version: string, provider: string}
  * @returns 
  */
-export const get = async (name, options = { destination: 'node_modules'}) => {
-  if (options.provider) return providers[options.provider].get(name, options.destination)
-
+export const get = async (name, options = { packageJson: {}, destination: 'node_modules' }) => {
+  if (!options.destination) options.destination =  'node_modules'
+  if (options.provider && !availableProviders.includes(options.provider)) throw new Error(`unsupported provider, got ${options.provider}.\n\navailableProviders:\n  -${availableProviders.join('\n  -')}`)
+  if (options.provider) {
+    const result = await providers[options.provider].get(name, options.destination)
+    if (result) return providerResult(name, options.destination, options.packageJson)
+    else throw new Error(`nothing found for ${name}`)
+  }
+  
   const iterate = async function* (name, destination) {
     for (const provider of Object.values(providers)) {
-      try {        
+      try {
         const data = await provider.get(name, destination)
         yield data
       } catch (error) {
@@ -25,8 +48,8 @@ export const get = async (name, options = { destination: 'node_modules'}) => {
   const iterator = iterate(name, options.destination)
   
   for await (const data of iterator) {
-    if (data) return data
+    if (data) return providerResult(name, options.destination, options.packageJson)
   }
-  
-  
+
+  throw new Error(`nothing found for ${name}`)  
 }
--- test.js
+++ test.js
@@ -4,3 +4,19 @@ console.log(availableProviders);
 
 const result = await get('@leofcoin/standards')
 console.log(result);
+
+try {
+  const badProvider = await get('@leofcoin/standards', {provider: 'my-provider'})
+  console.log('badProvider');
+  process.exit(1)
+} catch (error) {
+  
+}
+
+try {
+  const goodProvider = await get('@leofcoin/standards', {provider: 'npm'})
+} catch (error) {
+  console.log('good-provider');
+  process.exit(1)
+}
+process.exit(0)
